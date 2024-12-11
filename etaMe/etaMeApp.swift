28d33b809c607ad81110a7af9bdac90475311b35
calixteair <calixte.reymond@laposte.net>
2024-12-11 16:21:47 +0100

    Initial Commit

---
+++ etaMe/etaMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  etaMeApp.swift
+//  etaMe
+//
+//  Created by reymond calixte on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct etaMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
