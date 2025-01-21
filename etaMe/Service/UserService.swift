100160623277dad437e0298dc9462d1f3ad1c238
Zeky69 <zek.akburakgmail.com>
2025-01-21 21:53:47 +0100

    profil

---
+++ etaMe/Service/UserService.swift
@@ -0,0 +1,18 @@
+//
+//  UserService.swift
+//  EatMe
+//
+//  Created by maky on 21/01/2025.
+//
+
+import SwiftUI
+
+struct UserService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserService()
+}
---
+++ etaMe/View/EditProfilView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfilView.swift
+//  EatMe
+//
+//  Created by maky on 21/01/2025.
+//
+
+import SwiftUI
+
+struct EditProfilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfilView()
+}
